2f2b1ec3ddc96ac048b9e0c6a43238fc72ececbe
oliver <u1964368@unimail.hud.ac.uk>
2023-11-26 23:50:57 +0000

    Add project files.

---
+++ Calculator/BasicCalculations.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    internal class BasicCalculations
+    {
+        float num1 = 0;
+        float num2 = 0;
+        float sum = 0;
+
+        //BASIC CALCULATIONS
+        public void Addition()
+        {
+            Console.WriteLine("\nEnter First Number : ");
+            num1 = float.Parse(Console.ReadLine());
+            Console.WriteLine("Enter Second Number : ");
+            num2 = float.Parse(Console.ReadLine());
+
+            sum = num1 + num2;
+
+            Console.WriteLine("The Total is : {0}\n", sum);
+        }
+        public void Subtract()
+        {
+            Console.WriteLine("\nEnter First Number : ");
+            num1 = float.Parse(Console.ReadLine());
+            Console.WriteLine("Enter Second Number : ");
+            num2 = float.Parse(Console.ReadLine());
+
+            sum = num1 - num2;
+
+            Console.WriteLine("The Total is : {0}\n", sum);
+        }
+        public void Multiply()
+        {
+            Console.WriteLine("\nEnter First Number: ");
+            num1 = float.Parse(Console.ReadLine());
+            Console.WriteLine("Enter Second Number: ");
+            num2 = float.Parse(Console.ReadLine());
+
+            sum = num1 * num2;
+
+            Console.WriteLine("The Total is : {0}\n", sum);
+        }
+        public void Divide()
+        {
+            Console.WriteLine("\nEnter First Number: ");
+            num1 = float.Parse(Console.ReadLine());
+            Console.WriteLine("Enter Second Number: ");
+            num2 = float.Parse(Console.ReadLine());
+
+            sum = num1 / num2;
+
+            Console.WriteLine("The Total is : {0}\n", sum);
+        }
+
+        //ADVANCED CALCULATIONS
+        public void Square()
+        {
+            Console.WriteLine("\nEnter the Number you want to be Squared: ");
+            num1 = float.Parse(Console.ReadLine());
+
+            sum = num1 * num1;
+
+            Console.WriteLine("The Total is : {0}\n", sum);
+        }
+
+        public void SquareRoot()
+        {
+            Console.WriteLine("\nEnter the Number you want the Square Root of: ");
+            num1 = float.Parse(Console.ReadLine());
+
+            sum = (float)Math.Sqrt(num1);
+
+            Console.WriteLine("The Total is : {0}\n", sum);
+        }
+    }
+}
---
+++ Calculator/Program.cs
@@ -0,0 +1,87 @@
+﻿using Calculator;
+
+//Declaration Area
+int selectedAction;
+bool keepGoing = true;
+char y;
+
+BasicCalculations calculations = new BasicCalculations();
+while (keepGoing)
+{
+//Calculator Main Body
+Console.WriteLine("Pick an option below:");
+Console.WriteLine("1. Addition");
+Console.WriteLine("2. Subtract");
+Console.WriteLine("3. Multiply");
+Console.WriteLine("4. Divide");
+Console.WriteLine("5. Advanced Options");
+Console.WriteLine("\n6. Exit\n");
+
+selectedAction = int.Parse(Console.ReadLine());
+
+//Activate Calculator based on user input
+
+    switch (selectedAction)
+    {
+        case 1:
+            calculations.Addition();
+            break;
+
+        case 2:
+            calculations.Subtract();
+            break;
+
+        case 3:
+            calculations.Multiply();
+            break;
+
+        case 4:
+            calculations.Divide();
+            break;
+
+        case 5:
+            Advanced();
+            break;
+
+        case 6:
+            keepGoing = false;
+            break;
+
+        default:
+            Console.Write("Invalid Input, Exiting");
+            break;
+    }
+}
+//Advanced Options
+void Advanced()
+{
+    Console.WriteLine("\nPick an option below:");
+    Console.WriteLine("1. Squared");
+    Console.WriteLine("2. Square Root");
+    Console.WriteLine("\n3. Back");
+    Console.WriteLine("4. Exit\n");
+
+    selectedAction = int.Parse(Console.ReadLine());
+
+    switch (selectedAction)
+    {
+        case 1:
+            calculations.Square();
+            break;
+
+        case 2:
+            calculations.SquareRoot();
+            break;
+
+        case 3:
+            break;
+
+        case 4:
+            keepGoing = false;
+            break;
+
+        default:
+            Console.Write("Invalid Input, Exiting");
+            break;
+    }
+}
